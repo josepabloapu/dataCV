daafa624e7f0be91de315c3d0410c1b60a67fd1d
Jose Pablo Apú <josepabloapu@gmail.com>
2013-10-02 13:06:01 -0600

    add comments

--- proyecto/include/mysqplot.hh
+++ proyecto/include/mysqplot.hh
@@ -28,26 +28,39 @@ public:
 
 	Mysqplot(string);
 	~Mysqplot();
-  
-	bool conn(const char*,const char*,const char*,const char*);
+  	///connects to mysql server
+	bool conn(const char*,const char*,const char*,const char*); 
+	///
 	bool fill_vector(int,int, const char*, vector<double> &);
+	///
 	bool fill_vector(const char*, vector<double> &);
+	///fills a vector of aleatory values from -delta to +delta, where delta is the int parameter
 	bool fill_vector(const char*, vector<double> &, int);
+	///obtain the number of lines(rows) of a determined table
 	int get_lines();
+	///obtain the number of fields(columns) of a determined table
 	int get_cols();
-  
+  	/// obtain mean value of a field
 	float mean(const char*);
+	/// obtain variance value of a field
 	float variance(const char*);
+	/// obtain standar deviation value of a field
 	float standard_deviation(const char*);
-
+	///
 	bool histogram();
+	/// displays a gaussian distributions of a field
 	bool gaussian_distribution(const char*);
+	/// displays a jitterplot of a field
 	bool jitterplot(const char*, int);
+	/// displays a scatterplot of a fields, supposed to be x-y
 	bool scatterplot(const char*, const char*);
+	/// displays a kernel density estimates function of a fields
 	bool kde(const char*,float=1.0);
+	/// displays a kernel density estimates function of a fields
 	bool pdf(const char*,bool=false);
+	/// displays a cumulative distribution function of a fields
 	bool cdf(const char*);
-	
+	///
 	string gaussian_distribution_function(const char*);
 };
 
